6d8cc91579df1efb879e105628a477b6589e45aa
Zoe Ames <zoeames@gmail.com>
2020-03-21 10:52:54 -0500

    added login button

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,13 @@
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
+
+import auth from './components/auth/auth';
+
 import '../styles/main.scss';
 
 const init = () => {
-  console.error('ITS CATURDAY!!!!');
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  auth.loginButton();
 };
 
 init();
