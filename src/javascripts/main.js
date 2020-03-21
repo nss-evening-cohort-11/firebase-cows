333d4070a142021d0be0a666e02a31c7274fb5f8
Zoe Ames <zoeames@gmail.com>
2020-03-21 11:14:24 -0500

    added logout button

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,6 +3,7 @@ import apiKeys from './helpers/apiKeys.json';
 
 import authData from './helpers/data/authData';
 import auth from './components/auth/auth';
+import myNavbar from './components/myNavbar/myNavbar';
 
 import '../styles/main.scss';
 
@@ -10,6 +11,7 @@ const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   authData.checkLoginStatus();
   auth.loginButton();
+  myNavbar.logoutEvent();
 };
 
 init();
