27d617770ca474f78ddf6996e92f50115d039819
Zoe Ames <zoeames@gmail.com>
2020-03-23 19:21:40 -0500

    created promise and displaying cows

--- src/javascripts/components/pasture/pasture.js
+++ src/javascripts/components/pasture/pasture.js
@@ -1,8 +1,19 @@
 import cowData from '../../helpers/data/cowData';
+import utils from '../../helpers/utils';
+import cowComponent from '../cow/cow';
 
 const buildCows = () => {
   cowData.getCows()
-    .then((response) => console.error('get cows worked!!!', response.data))
+    .then((cows) => {
+      let domString = '';
+      domString += '<h2 class="text-center">Pasture</h2>';
+      domString += '<div class="d-flex flex-wrap">';
+      cows.forEach((cow) => {
+        domString += cowComponent.cowMaker(cow);
+      });
+      domString += '</div>';
+      utils.printToDom('pasture', domString);
+    })
     .catch((err) => console.error('get cows broke', err));
 };
 
--- src/javascripts/helpers/data/cowData.js
+++ src/javascripts/helpers/data/cowData.js
@@ -3,6 +3,18 @@ import apiKeys from '../apiKeys.json';
 
 const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
-const getCows = () => axios.get(`${baseUrl}/cows.json`);
+const getCows = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/cows.json`)
+    .then((response) => {
+      const demCows = response.data;
+      const cows = [];
+      Object.keys(demCows).forEach((cowId) => {
+        demCows[cowId].id = cowId;
+        cows.push(demCows[cowId]);
+      });
+      resolve(cows);
+    })
+    .catch((err) => reject(err));
+});
 
 export default { getCows };
