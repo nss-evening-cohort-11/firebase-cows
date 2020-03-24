624c985eef321112911cf2e5024739d0f43dfa4a
Zoe Ames <zoeames@gmail.com>
2020-03-23 19:25:25 -0500

    added farmhouse div

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -5,6 +5,7 @@ import pasture from '../../components/pasture/pasture';
 
 const authDiv = $('#auth');
 const pastureDiv = $('#pasture');
+const farmhouseDiv = $('#farmhouse');
 const logoutButton = $('#navbar-logout-button');
 
 const checkLoginStatus = () => {
@@ -13,12 +14,14 @@ const checkLoginStatus = () => {
       // person is logged in
       authDiv.addClass('hide');
       pastureDiv.removeClass('hide');
+      farmhouseDiv.removeClass('hide');
       logoutButton.removeClass('hide');
       pasture.buildCows();
     } else {
       // person is NOT logged in
       authDiv.removeClass('hide');
       pastureDiv.addClass('hide');
+      farmhouseDiv.addClass('hide');
       logoutButton.addClass('hide');
     }
   });
